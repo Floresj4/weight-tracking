a2e9469a0c9bcb265cc3c8d0c25306bf02d18236
Jason Flores <floresj4@gmail.com>
2023-03-18 09:44:47 -0400

    initial commit

---
+++ src/main/java/com/flores/dev/batch/weight/BatchWeight.java
@@ -0,0 +1,12 @@
+package com.flores.dev.batch.weight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchWeight {
+
+	public static void main(String args[]) throws Exception {
+		SpringApplication.run(BatchWeight.class, args);
+	}
+}
