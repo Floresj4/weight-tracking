c4b72bb91abb2fc12d603b016484c392238d40f2
Jason Flores <floresj4@gmail.com>
2023-03-24 23:18:05 -0400

    cleaning

--- api/weights-api.py
+++ api/weights-api.py
@@ -5,5 +5,3 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog = 'Weight Tracking Access', description='Database interactions')
     parser.add_argument('path', default = '/', type = str)
     args = parser.parse_args()
-
-    print(args)
