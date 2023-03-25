ed068806d5e7e4e304e76a635ff6e33c1f7586a2
Jason Flores <floresj4@gmail.com>
2023-03-24 23:28:51 -0400

    starting api

--- api/weights-api.py
+++ api/weights-api.py
@@ -1,5 +1,26 @@
 import argparse
 
+def request_handler(path: str, req_payload: dict):
+
+    if(path == '/'):
+        
+        return {}
+    
+    # not sure this is the endpoint I want yet
+    elif(path == '/year')
+
+        year = 2023
+        data = get_data_by_year(year)
+
+        return data
+
+
+    return {}
+
+
+def get_data_by_year(year: int):
+    pass
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(prog = 'Weight Tracking Access', description='Database interactions')
