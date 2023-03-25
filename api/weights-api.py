0263a6917ecbfe04348e2648cd162b57a9338613
Jason Flores <floresj4@gmail.com>
2023-03-24 23:17:29 -0400

    starting argparse

---
+++ api/weights-api.py
@@ -0,0 +1,9 @@
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(prog = 'Weight Tracking Access', description='Database interactions')
+    parser.add_argument('path', default = '/', type = str)
+    args = parser.parse_args()
+
+    print(args)
