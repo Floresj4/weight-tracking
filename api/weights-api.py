6a827bcb3ce4a04a92677af93973fd073fde5e45
Jason Flores <floresj4@gmail.com>
2023-03-27 21:37:53 -0400

    connect and select data by year

--- api/weights-api.py
+++ api/weights-api.py
@@ -1,6 +1,8 @@
 import os
 import logging, argparse
 
+import MySQLdb
+
 def initialize_logger(name: str = __name__):
     '''
     initialize logger
@@ -18,26 +20,52 @@ logger = initialize_logger()
 
 def request_handler(path: str, req_payload: dict):
 
-    if(path == '/'):
-        
-        return {}
-    
-    # not sure this is the endpoint I want yet
-    elif(path == '/year'):
+    dbconnect = None
 
-        year = 2023
-        data = get_data_by_year(year)
+    try:
 
-        return data
+        dbconnect = MySQLdb.connect(host = '127.0.0.1', user = 'root', passwd = '1234', db = 'weight_tracking')
 
+        if(path == '/'):
+
+            return {}
+
+        # not sure this is the endpoint I want yet
+        elif(path == '/year'):
+
+            year = 2023
+            data = get_data_by_year(dbconnect, year)
+
+            return data
+
+    finally:
+
+        if dbconnect:
+            dbconnect.close()
 
     return {}
 
 
-def get_data_by_year(year: int):
+def get_data_by_year(connection, year: int):
+    '''
+    Collect data for a given year
+    '''
     logger.debug(f'Collecting data for year {year}')
     
-    return {}
+    cursor = connection.cursor()
+    cursor.execute(f'select * from weight_entries where year(entry_date) = {year}')
+
+    yearly_data = []
+    for _date, _value in cursor.fetchall():
+        yearly_data.append({
+            'entry_date': _date,
+            'entry_value': _value
+            })
+
+    return {
+        'year': year,
+        'data': yearly_data
+    }
 
 
 if __name__ == '__main__':
