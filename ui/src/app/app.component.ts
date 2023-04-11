edd2e9581ca7397ce37b4d19324924284d531162
Jason Flores <floresj4@gmail.com>
2023-04-11 07:46:40 -0400

    months for presentation

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -21,6 +21,10 @@ export class AppComponent {
 
   monthlyAvg: WeightMonthlyAvg = <WeightMonthlyAvg>{}
 
+  months: string[] = ['January', 'February', 'March',
+    'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December']
+
   constructor(private data: DataService) {
 
     this.getAvailableYears()
