334d05dffb77d8598770992c024ccae0a719b06d
Jason Flores <floresj4@gmail.com>
2023-11-15 22:50:25 -0500

    more data points

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -50,18 +50,18 @@ export class AppComponent {
     const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
     this.dataz = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: this.month,
         datasets: [
             {
-                label: 'First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 40],
+                label: 'Average',
+                data: [65, 59, 80, 81, 56, 55, 40, 33, 54, 84, 21, 90],
                 fill: false,
                 borderColor: documentStyle.getPropertyValue('--blue-500'),
                 tension: 0.4
             },
             {
-                label: 'Second Dataset',
-                data: [28, 48, 40, 19, 86, 27, 90],
+                label: 'High',
+                data: [28, 48, 40, 19, 86, 27, 90, 54, 21, 76, 23, 92],
                 fill: false,
                 borderColor: documentStyle.getPropertyValue('--pink-500'),
                 tension: 0.4
