cd0a59afc286e562abf1648b8c13c9aa5cc1a4ca
Jason Flores <floresj4@gmail.com>
2024-12-18 20:45:11 -0500

    create a simple dynamo table

--- spring-dynamo/src/main/java/com/flores/dev/dynamo/SpringDynamo.java
+++ spring-dynamo/src/main/java/com/flores/dev/dynamo/SpringDynamo.java
@@ -1,65 +1,84 @@
 package com.flores.dev.dynamo;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import java.net.URI;
+import java.util.Optional;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jdk.internal.org.jline.utils.Log;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.waiters.WaiterResponse;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableResponse;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableResponse;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
+import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
+import software.amazon.awssdk.services.dynamodb.waiters.DynamoDbWaiter;
 
+@Slf4j
 public class SpringDynamo {
 
-	private static final String amazonDynamoDBEndpoint = "http://localhost:8000";
+	private static final String LOCAL_DB_ENDPOINT = "http://localhost:8000";
 
-	private static final String amazonAWSAccessKey = "Sample";
+	private static final String AMAZON_AWS_ACCESS_KEY = "Sample";
 
-	private static final String amazonAWSSecretKey = "Sample";
-
-	@Getter
-	@NoArgsConstructor
-	@AllArgsConstructor
-	@Builder(setterPrefix = "with")
-	@DynamoDBTable(tableName = "WeightTable")
-	public static class WeightItem {
-
-		@DynamoDBHashKey
-		private String id;
-		
-		@DynamoDBAttribute
-		private String date;
-
-		@DynamoDBAttribute
-		private Double value;
-	}
-
-	public static AmazonDynamoDB amazonDynamoDb() {
-		return AmazonDynamoDBClientBuilder.standard()
-				.withCredentials(amazonAWSCredentials())
-				.withEndpointConfiguration(new AwsClientBuilder
-						.EndpointConfiguration("http://localhost:8000", "us-west-1"))
-				.build();
-	}
-
-	public static AWSCredentialsProvider amazonAWSCredentials() {
-		return new AWSStaticCredentialsProvider(new 
-				BasicAWSCredentials(amazonAWSAccessKey, 
-						amazonAWSSecretKey));
-	}
+	private static final String AMAZON_AWS_SECRET_KEY = "Sample";
 
 	public static void main(String args[]) throws Exception {
 
-		AmazonDynamoDB amazonDynamoDb = amazonDynamoDb();
-
-		DynamoDBMapper dynamoMapper = new DynamoDBMapper(amazonDynamoDb);
-
+		DynamoDbClient client = DynamoDbClient.builder()
+				.credentialsProvider(StaticCredentialsProvider
+						.create(AwsBasicCredentials.create(AMAZON_AWS_ACCESS_KEY, 
+								AMAZON_AWS_SECRET_KEY)))
+				.region(Region.US_EAST_1)
+				.endpointOverride(new URI(LOCAL_DB_ENDPOINT))
+				.build();
+		
+		DynamoDbWaiter waiter = client.waiter();
+		
+		String tableName = "guids";
+		String key = "id";
+		
+		CreateTableRequest createTable = CreateTableRequest.builder()
+				.attributeDefinitions(AttributeDefinition.builder()
+						.attributeName(key)
+						.attributeType(ScalarAttributeType.S)
+						.build())
+				.keySchema(KeySchemaElement.builder()
+						.attributeName(key)
+						.keyType(KeyType.HASH)
+						.build())
+				.provisionedThroughput(ProvisionedThroughput.builder()
+						.readCapacityUnits(10L)
+						.writeCapacityUnits(10L)
+						.build())
+				.tableName(tableName)
+				.build();
+		
+		try {
+			CreateTableResponse response = client.createTable(createTable);
+			DescribeTableRequest describeTable = DescribeTableRequest.builder()
+					.tableName(tableName)
+					.build();
+			
+			//wait until DynamoDb is finished
+			WaiterResponse<DescribeTableResponse> waiterResponse = waiter.waitUntilTableExists(describeTable);
+			waiterResponse.matched()
+					.response()
+					.ifPresent(System.out::println);;
+			
+			String newTableName = response.tableDescription().tableName();
+			System.out.println(newTableName);
+		}
+		catch(DynamoDbException e) {
+			Log.error(e);
+		}
 	}
 }
