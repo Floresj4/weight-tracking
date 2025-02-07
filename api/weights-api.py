a755fe00ecb24ede6e05616dffdc10d8b9c64f4e
Jason Flores <floresj4@gmail.com>
2025-02-06 20:57:55 -0500

    converting to use sample json

--- api/weights-api.py
+++ api/weights-api.py
@@ -1,8 +1,12 @@
 import os, re, json, sys
+import urllib.parse
 import logging, argparse
 
-import boto3
 from botocore.config import Config
+
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
 
 config = Config(
    connect_timeout = 3.0,
@@ -35,10 +39,16 @@ path_data_by_year_trend = re.compile(r'\/year\/(\d{4})\/trend')
 path_data_by_year_monthly_avg = re.compile(r'\/year\/(\d{4})\/avg')
 path_data_by_year = re.compile(r'\/year\/(\d{4})')
 
-def request_handler(path: str, req_payload: dict):
+def request_handler(event: str, context: dict):
+
+    path = event['rawPath']
+    queryString = event['rawQueryString']
 
     if(match_result := path_data_new.fullmatch(path)):
         put_entry(req_payload)
+    
+    elif(match_result := path_data_by_year.fullmatch(path)):
+        get_data_by_year(path)
 
 
 def put_entry(payload):
@@ -95,7 +105,16 @@ def get_data_by_year(match_result):
 
     e.g., /year/2021
     '''
-    pass
+
+    logger.info(f'get_data_by_year: {match_result}')
+
+    table = dynamodb.Table('Weights')
+    response = table.query(
+        KeyConditionExpression = Key('pk').eq('id#1') \
+            & Key('sk').begins_with('cart#'),
+        FilterExpression = Attr('name').eq('SomeName')
+    )
+
 
 
 def get_payload_from_input(input):
@@ -118,16 +137,22 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(prog = 'Weight Tracking Access', description='Database interactions by request routes.')
     parser.add_argument('path', default = '/', type = str, help = 'Path to execute. e.g., /year/2023')
-    parser.add_argument('--payload', type = str, help = 'Comma-separate values of payload data - USERGUID,DATE,VALUE e.g., 9842-1232-2321-0923,2025-01-01,175.0')
+    parser.add_argument('--body', type = str, help = 'Comma-separate values of post data - USERGUID,DATE,VALUE e.g., 9842-1232-2321-0923,2025-01-01,175.0')
     parser.add_argument('--indent', default = None, type = int, help = 'Indentation size on json pretty print.  Default None to be less verbose during dev/test.')
     args = parser.parse_args()
 
     # parse simple input into a request payload
-    payload = get_payload_from_input(args.payload) \
-        if args.payload else {}
+    post_body = get_payload_from_input(args.body) \
+        if args.body else {}
+
+    event = {}
+    with open('./test/api-gateway-http-api.json', 'r') as api_sample:
+        event = json.loads(api_sample)
+
+    logger.info(json.dumps(event))
 
     # verify path and response
-    response = request_handler(args.path, payload)
-    json.dump(response, sys.stdout
-              , indent = args.indent
-              , default = str)
+    # response = request_handler(args.path, payload)
+    # json.dump(response, sys.stdout
+    #           , indent = args.indent
+    #           , default = str)
