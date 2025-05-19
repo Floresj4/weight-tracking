451f1e936460f10ecfa0fe29b2cbad2931a94f4b
Jason Flores <floresj4@gmail.com>
2025-05-18 21:48:54 -0400

    creating weight entry operations

---
+++ batch/src/main/java/com/flores/dev/dynamo/WeightEntryOperations.java
@@ -0,0 +1,55 @@
+package com.flores.dev.dynamo;
+
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
+import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
+
+@Slf4j
+public class WeightEntryOperations implements DynamoOperations {
+
+	public static final String ATTRIBUTE_GUID = "guid";
+	public static final String ATTRIBUTE_ENTRY_DATE = "entry-date";
+	public static final String ATTRIBUTE_VALUE = "value";
+	
+	public CreateTableRequest createTable() {
+		log.info("Creating Weight table request");
+
+		//define table attributes
+		AttributeDefinition guidAttribute = AttributeDefinition.builder()
+				.attributeType(ScalarAttributeType.S)
+				.attributeName(ATTRIBUTE_GUID)
+				.build();
+		
+		AttributeDefinition dateAttribute = AttributeDefinition.builder()
+				.attributeType(ScalarAttributeType.S)
+				.attributeName(ATTRIBUTE_ENTRY_DATE)
+				.build();
+
+		//define table primary key attributes
+		KeySchemaElement partitionKey = KeySchemaElement.builder()
+				.keyType(KeyType.HASH)
+				.attributeName(ATTRIBUTE_GUID)
+				.build();
+		
+		KeySchemaElement sortKey = KeySchemaElement.builder()
+				.keyType(KeyType.RANGE)
+				.attributeName(ATTRIBUTE_ENTRY_DATE)
+				.build();
+		
+		String tableName = "Weights";
+		return CreateTableRequest.builder()
+				.attributeDefinitions(guidAttribute, dateAttribute)
+				.keySchema(partitionKey, sortKey)
+				.provisionedThroughput(ProvisionedThroughput.builder()
+						.readCapacityUnits(5L)
+						.writeCapacityUnits(5L)
+						.build())
+				.tableName(tableName)
+				.build();	
+	}
+
+}
