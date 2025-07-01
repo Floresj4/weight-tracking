2d1bfd42ff668e868a598ba38e561c8ebeef25b3
Jason Flores <floresj4@gmail.com>
2025-06-30 22:05:05 -0400

    register and include header component

--- weight-tracking-ui/src/app/app.component.ts
+++ weight-tracking-ui/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './component/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class App {
-  protected title = 'weight-tracking-ui';
 }
