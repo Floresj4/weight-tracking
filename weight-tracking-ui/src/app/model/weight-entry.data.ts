9a56da35fb988cc79f5920314282be3cb59f5f3d
Jason Flores <floresj4@gmail.com>
2025-07-01 21:50:42 -0400

    create sample data

---
+++ weight-tracking-ui/src/app/model/weight-entry.data.ts
@@ -0,0 +1,12 @@
+import { WeightEntry } from "./weight-entry.model";
+
+export const SAMPLE_ENTRY_DATA: WeightEntry[] = [
+    {
+        value: 160,
+        date: "2001-01-01"
+    },
+    {
+        value: 159,
+        date: '2001-01-08'
+    }
+];
