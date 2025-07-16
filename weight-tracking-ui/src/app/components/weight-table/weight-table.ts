d1aee459c0c7bba04819067d252490cd67396a7e
Jason Flores <floresj4@gmail.com>
2025-07-15 22:13:47 -0400

    new component

---
+++ weight-tracking-ui/src/app/components/weight-table/weight-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weight-table',
+  imports: [],
+  templateUrl: './weight-table.html',
+  styleUrl: './weight-table.scss'
+})
+export class WeightTable {
+
+}
