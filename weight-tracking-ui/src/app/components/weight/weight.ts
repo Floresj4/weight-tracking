758aba67d4eb72717227e32881d248f7d74e3f32
Jason Flores <floresj4@gmail.com>
2025-07-15 21:24:04 -0400

    add required input

--- weight-tracking-ui/src/app/components/weight/weight.ts
+++ weight-tracking-ui/src/app/components/weight/weight.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { WeightEntry } from '../../model/weight-entry.model';
 
 @Component({
   selector: 'app-weight',
+  standalone: true,
   imports: [],
   templateUrl: './weight.html',
   styleUrl: './weight.scss'
 })
 export class Weight {
 
+  entry = input.required<WeightEntry>()
 }
