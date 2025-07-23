7d5562372385fb9515b6236a612a593ec7ed9488
Jason Flores <floresj4@gmail.com>
2025-07-22 21:36:56 -0400

    starting component for new entries

---
+++ weight-tracking-ui/src/app/weight-entries/new-weight/new-weight.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-weight',
+  imports: [],
+  templateUrl: './new-weight.html',
+  styleUrl: './new-weight.scss'
+})
+export class NewWeight {
+
+}
