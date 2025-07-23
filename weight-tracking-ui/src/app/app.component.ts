6f011c8bc6603e5563160cef6d14b9ccf4de5fb2
Jason Flores <floresj4@gmail.com>
2025-07-22 21:48:09 -0400

    renaming

--- weight-tracking-ui/src/app/app.component.ts
+++ weight-tracking-ui/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Header } from './components/header/header';
 import { SAMPLE_ENTRY_DATA } from './model/weight-entries.data';
 import { WeightTable } from './components/weight-table/weight-table';
 import { WeightEntry } from './model/weight-entry.model';
+import { WeightEntryNew } from './weight-entries/new-weight/weight-entry-new';
 
 import { SAMPLE_USERS_DATA } from './model/users.data';
 import { UserModel} from './model/user.model';
@@ -13,7 +14,7 @@ import { User } from "./components/user/user";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, User, WeightTable],
+  imports: [RouterOutlet, Header, User, WeightTable, WeightEntryNew],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -24,6 +25,8 @@ export class App {
   weightEntries: WeightEntry[] = SAMPLE_ENTRY_DATA;
 
   selectedUser = signal<UserModel>(<UserModel>{})
+
+  showNewEntry: boolean = false;
 
   constructor() {
     this.selectedUser.set(SAMPLE_USERS_DATA[0])
---
+++ weight-tracking-ui/src/app/weight-entries/new-weight/weight-entry-new.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weight-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './weight-entry-new.html',
+  styleUrl: './weight-entry-new.scss'
+})
+export class WeightEntryNew {
+
+}
