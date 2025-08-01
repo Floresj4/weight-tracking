1a5f7c2f96f4d4e3a82f413c13516ae95d2f5122
Jason Flores <floresj4@gmail.com>
2025-08-01 13:44:57 -0400

    using content projection for stats

--- weight-tracking-ui/src/app/weight-entries/weight-stat/weight-stat.ts
+++ weight-tracking-ui/src/app/weight-entries/weight-stat/weight-stat.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
+import { Card } from "../../components/shared/card/card";
 
 @Component({
   selector: 'app-weight-stat',
   standalone: true,
-  imports: [],
+  imports: [Card],
   templateUrl: './weight-stat.html',
   styleUrl: './weight-stat.scss'
 })
